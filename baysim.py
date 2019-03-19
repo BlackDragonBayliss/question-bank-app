c0c3e480551b23b9c757022dfaec577af23b7c98
Wallace Carr <wallacecarr4@gmail.com>
2019-03-18 19:25:02 -0600

    update testing

--- baysim.py
+++ baysim.py
@@ -8,8 +8,10 @@ def main():
     questionObjectManager = QuestionObjectManager()
     questionComposite = processParseQuestionBank(stringText)
 
-    print("question Composite: "+str(questionComposite))
+    # print("question Composite: "+str(questionComposite))
     questionObjectManager.setQuestionList(questionComposite)
+    print("question Composite in QOM: " + str(questionObjectManager.getQuestionList()))
+
 
     # possibleAnswerList = questionObjectComposite[0].getAnswerListComposite()
     # firstQuestion = questionObjectComposite[0]
@@ -19,7 +21,7 @@ def main():
     # print(questionList)
 
     #construct question, randomizing answers
-    questionObjectManager.createTest(False)
+    questionObjectManager.createTest(1)
 
 
 
@@ -35,129 +37,6 @@ def main():
 
 
 
-
-    root = Tk()
-    root.geometry('1400x800')
-
-    questionlist = ["A", "B", "C"]
-    selectedAnswerList = []
-    textAnswerList = []
-    buttonList = []
-
-
-    def ShowChoice():
-        for selectedAnswer in selectedAnswerList:
-            print(selectedAnswer.get())
-
-    for counter, choiceText in enumerate(questionlist, 1):
-        Label(root, text=choiceText).grid(row=counter, column=0)
-        var = IntVar()
-        for i in range(1, 2):
-            textAnswerList.append([str(counter),choiceText])
-            # print("I value created: "+str(i))
-            button = Radiobutton(root, variable=var, value=i, command=ShowChoice)
-            button.grid(row=counter, column=i)
-            buttonList.append(button)
-            selectedAnswerList.append(var)
-
-
-    correctAnswersList =  []# correctAnswersList
-
-    answerMatchingComposite = []
-
-    def confirmAnswer():
-        # create answerBooleanList to be appended later to textAnswerList
-        answerBooleanList = []
-        selectedAnswerIndex = 0
-        for selectedAnswer in selectedAnswerList:
-            # print("hit "+str(selectedAnswer))
-            # for each selected answer, if case met return true, continue
-            # else return false
-            if(selectedAnswer.get() == 1):
-                # print("true at index: "+str(selectedAnswerIndex))
-                answerBooleanList.append("1")
-            else:
-                answerBooleanList.append("0")
-            selectedAnswerIndex += 1
-
-
-
-        #append is selected
-        appendTrueSelectedValueCount = 0
-        for textAnswer in textAnswerList:
-            if (len(textAnswer) == 3):
-                print("I'm at index 3")
-                del textAnswer[2:3]
-            textAnswer.append(answerBooleanList[appendTrueSelectedValueCount])
-            appendTrueSelectedValueCount += 1
-        # print(selectedAnswerList)
-        print(textAnswerList)
-        print(str(len(textAnswerList)))
-
-
-
-
-
-        # instantiate is matching list
-        for textAnswer in textAnswerList:
-            correctAnswerIndex = 0
-            answerMatchingList = [textAnswer]
-            answerMatchingComposite.append(answerMatchingList)
-            selectedAnswerText = textAnswer[1]
-
-            for correctAnswer in correctAnswerList:
-                if(selectedAnswerText == correctAnswer):
-                    isAnswerMatching = True
-                else:
-                    isAnswerMatching = False
-                answerMatchingList.append(isAnswerMatching)
-        print(textAnswerList)
-        print(answerMatchingComposite)
-
-        # calculate are answers correct
-
-
-        # filter isAnswerMatchingList, if isNotTrue found then tag answer as incorrect.
-        # Case in which success? Case in which fail?
-        # Given that all answers in correct answer list criteria need to be met, or wrong.
-            # Also not too many answers given, or wrong answers selected.
-            # Perhaps finding where criteria is not met, and if found then wrong.
-
-            # iterate through corr
-                # print(correctAnswer)
-
-        selectedChoices = []
-        for selectedAnswer in selectedAnswerList:
-            # print(selectedAnswer.get())
-            selectedChoices.append(selectedAnswer.get())
-        print(selectedChoices)
-
-        # selectedChoices = [0,0,0,0,1,1,0,0]
-        answersList = [1,1,1,0,0,0,0,0]
-
-        # index = 0
-        # while(index < len(selectedChoices)):
-        #     if(selectedChoices[index] == answersList[index]):
-        #         print("true")
-        #     else:
-        #         print("false")
-        #     index += 1
-
-
-    def deselectAnswers():
-        for selectedAnswer in selectedAnswerList:
-            selectedAnswer.set(0)
-
-        print(textAnswerList)
-        print(str(len(textAnswerList)))
-
-    deselectOptionsButton = Button(text="Deselect Answers", command=deselectAnswers)
-    deselectOptionsButton.place(x=70, y=200)
-
-    confirmAnswerButton = Button(text="Confirm Answer", command=confirmAnswer)
-    confirmAnswerButton.place(x=70, y=150)
-
-    root.mainloop()
 
 
 
@@ -262,14 +141,20 @@ class QuestionObject:
 
 class QuestionObjectManager:
     def __init__(self):
-        ranomizedQuestionsList = []
-        questionList = []
+        self.ranomizedQuestionsList = []
+        self.questionList = []
 
     #start on first question or randomize questions.
     #saving state of test completed
     #sampling or entirety
     #I want a 50 queston sample
     #I want to iterate through entire list
+
+    def setQuestionList(self, questionList):
+        self.questionList = questionList
+    def getQuestionList(self):
+        return self.questionList
+
 
     def createTest(self, caseType):
         if(0):
@@ -279,17 +164,21 @@ class QuestionObjectManager:
             # print("questionList: "+str(self.getQuestionList()))
             # This is wrong it needs to be one individual question per.... Ah needs to randomize questions first.
             # Set randomized list at another time.
+            # print("Question list1 " + str(self.getQuestionList()))
             self.shuffleQuestionList()
 
             testInstance = Test()
 
+            print("Question list1 "+ str(self.getQuestionList()))
+            testInstance.setQuestionList(self.getQuestionList())
+            #
+            print("Question list2 " + str(testInstance.getQuestionList()))
+            testInstance.getQuestionList()
 
-            testInstance.setQuestionBank(QuestionObjectManager.getQuestionList())
 
             testInstance.randomizeQuestionList()
-            testInstance.setCurrentQuestion()
-
-
+            # testInstance.setCurrentQuestion()
+            testInstance.startTest()
 
 
 
@@ -315,8 +204,13 @@ class QuestionObjectManager:
                 # print(question.getRandomizedList())
                 # print(question.getQuestionNumber())
 
+
+
     def shuffleQuestionList(self):
-        self.setQuestionList(shuffle(self.getQuestionList()))
+        list = self.getQuestionList()#[20, 16, 10, 5];
+        shuffle(list)
+        print("Reshuffled list : ", list)
+        self.setQuestionList(list)
 
     def calculateQuestionSuccess(self):
         self.associateQuestionAnswersWithRandomizedResults()
@@ -328,7 +222,7 @@ class QuestionObjectManager:
         # It knows the current question, randomized question list, answer list,
         # Need selected answer positioning.
 
-        self.getRanomizedQuestionsList()
+        self.getRanomizedQuestionList()
 
         #associate placement?
         #Or... check if match, check if match
@@ -337,29 +231,16 @@ class QuestionObjectManager:
                 # currently selected returned as a list.
 
 
-        #
-
-
-
-
-
-    def createQuestion(self):
-        pass
-
-    def setQuestionList(self, questionList):
-        self.questionList = questionList
-    def getQuestionList(self):
-        return self.questionList
     def setCurrentQuestion(self, questionObject):
         pass
-    def randomizeQuestions(self):
-        shuffledList = shuffle(self.getRanomizedQuestionList())
+    def randomizeQuestionList(self):
+        shuffledList = shuffle(self.getRandomizedQuestionList())
         print("shuffle: " + str(shuffledList))
         return shuffledList
 
-    def getRanomizedQuestionList(self):
-        return self.ranomizedQuestionsList
-    def setRanomizedQuestionList(self, randomizedQuestionList):
+    def getRandomizedQuestionList(self):
+        return self.randomizedQuestionsList
+    def setRandomizedQuestionList(self, randomizedQuestionList):
         self.randomizedQuestionList = randomizedQuestionList
 
     def randomizeQuestionAnswers(self, answerList):
@@ -372,7 +253,7 @@ class QuestionObjectManager:
 
     def setOriginalQuestionList(self, question):
         pass
-    def getOriginalQuestionList():
+    def getOriginalQuestionList(self):
         pass
 
 
@@ -394,10 +275,12 @@ class Test:
     def changeToNextQuestion(self):
         # Handled elsewhere to give functionality to go back to previous questions without adding to completed list issues.
         self.handleCompletedQuestionChange()
-        self.setCurrentQuestion()
+        #Set current question isAnswered
+        self.getCurrentQuestion().setIsAnswered(1)
+
+        # self.setCurrentQuestion()
     def handleCompletedQuestionChange(self):
         # If currentQuestion has already been added,
-
         pass
         # for questionObject
         # currentQuestionIndex
@@ -406,5 +289,123 @@ class Test:
         self.currentQuestion = currentQuestion
     def getCurrentQuestion(self):
         return self.currentQuestion
+
+
+
+    def startTest(self):
+
+        root = Tk()
+        root.geometry('1400x800')
+
+        questionlist = self.getQuestionList()#["A", "B", "C"]
+        selectedAnswerList = []
+        textAnswerList = []
+        buttonList = []
+
+        def ShowChoice():
+            for selectedAnswer in selectedAnswerList:
+                print(selectedAnswer.get())
+
+        for counter, choiceText in enumerate(questionlist, 1):
+            Label(root, text=choiceText).grid(row=counter, column=0)
+            var = IntVar()
+            for i in range(1, 2):
+                textAnswerList.append([str(counter), choiceText])
+                # print("I value created: "+str(i))
+                button = Radiobutton(root, variable=var, value=i, command=ShowChoice)
+                button.grid(row=counter, column=i)
+                buttonList.append(button)
+                selectedAnswerList.append(var)
+
+        correctAnswersList = []  # correctAnswersList
+
+        answerMatchingComposite = []
+
+        def confirmAnswer():
+            # create answerBooleanList to be appended later to textAnswerList
+            answerBooleanList = []
+            selectedAnswerIndex = 0
+            for selectedAnswer in selectedAnswerList:
+                # print("hit "+str(selectedAnswer))
+                # for each selected answer, if case met return true, continue
+                # else return false
+                if (selectedAnswer.get() == 1):
+                    # print("true at index: "+str(selectedAnswerIndex))
+                    answerBooleanList.append("1")
+                else:
+                    answerBooleanList.append("0")
+                selectedAnswerIndex += 1
+
+            # append is selected
+            appendTrueSelectedValueCount = 0
+            for textAnswer in textAnswerList:
+                if (len(textAnswer) == 3):
+                    print("I'm at index 3")
+                    del textAnswer[2:3]
+                textAnswer.append(answerBooleanList[appendTrueSelectedValueCount])
+                appendTrueSelectedValueCount += 1
+            # print(selectedAnswerList)
+            print(textAnswerList)
+            print(str(len(textAnswerList)))
+
+            # instantiate is matching list
+            for textAnswer in textAnswerList:
+                correctAnswerIndex = 0
+                answerMatchingList = [textAnswer]
+                answerMatchingComposite.append(answerMatchingList)
+                selectedAnswerText = textAnswer[1]
+
+                for correctAnswer in correctAnswerList:
+                    if (selectedAnswerText == correctAnswer):
+                        isAnswerMatching = True
+                    else:
+                        isAnswerMatching = False
+                    answerMatchingList.append(isAnswerMatching)
+            print(textAnswerList)
+            print(answerMatchingComposite)
+
+            # calculate are answers correct
+
+
+            # filter isAnswerMatchingList, if isNotTrue found then tag answer as incorrect.
+            # Case in which success? Case in which fail?
+            # Given that all answers in correct answer list criteria need to be met, or wrong.
+            # Also not too many answers given, or wrong answers selected.
+            # Perhaps finding where criteria is not met, and if found then wrong.
+
+            # iterate through corr
+            # print(correctAnswer)
+
+            selectedChoices = []
+            for selectedAnswer in selectedAnswerList:
+                # print(selectedAnswer.get())
+                selectedChoices.append(selectedAnswer.get())
+            print(selectedChoices)
+
+            # selectedChoices = [0,0,0,0,1,1,0,0]
+            answersList = [1, 1, 1, 0, 0, 0, 0, 0]
+
+            # index = 0
+            # while(index < len(selectedChoices)):
+            #     if(selectedChoices[index] == answersList[index]):
+            #         print("true")
+            #     else:
+            #         print("false")
+            #     index += 1
+
+        def deselectAnswers():
+            for selectedAnswer in selectedAnswerList:
+                selectedAnswer.set(0)
+
+            print(textAnswerList)
+            print(str(len(textAnswerList)))
+
+        deselectOptionsButton = Button(text="Deselect Answers", command=deselectAnswers)
+        deselectOptionsButton.place(x=70, y=200)
+
+        confirmAnswerButton = Button(text="Confirm Answer", command=confirmAnswer)
+        confirmAnswerButton.place(x=70, y=150)
+
+        root.mainloop()
 
 if __name__ == "__main__": main()
