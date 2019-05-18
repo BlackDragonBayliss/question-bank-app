2a010e6f6415aa82c65c9e17e37560104d2b24d1
Wallace Carr <wallacecarr4@gmail.com>
2019-05-17 23:08:41 -0600

    update test

--- Test.py
+++ Test.py
@@ -50,22 +50,6 @@ class Test:
         with open("my_pdf.pdf", "rb") as f:
             pdf = pdftotext.PDF(f)
         self.pdfText = "\n\n".join(pdf)
-        # If it's password-protected
-        # with open("secure.pdf", "rb") as f:
-        #     pdf = pdftotext.PDF(f, "secret")
-
-        # How many pages?
-        # print(len(pdf))
-
-        # Iterate over all the pages
-        # for page in pdf:
-        #     print(page)
-
-        # Read some individual pages
-        # print(pdf[5])
-        # print(pdf[1])
-
-        # Read all the text into one string
 
     def readQuestionBank(self):
         # f = open("demofile.txt", "r")
